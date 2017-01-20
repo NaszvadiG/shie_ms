a6b456924f1600606be63170ac90eb7a848d7a51
unknown <>
2017-01-20 17:06:10 +0900

    管理・公開それぞれのスーパーcontrollerクラスを作成

---
+++ application/controllers/admin/Dashbord.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashbord extends AdminController {
+
+
+	function __construct()
+	{
+		parent::__construct();
+		// something
+	}
+
+	function index(){
+		die('dashbord_index');
+	}
+}
---
+++ application/core/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * 管理画面の継承元クラス
+ *
+ * -----------------------------------------------------------------------------------
+ * 2017-1-20　add shie
+ * 
+ */
+class AdminController extends MY_Controller {
+
+
+
+
+	function __construct()
+	{
+		parent::__construct();
+		// something
+	}
+}
